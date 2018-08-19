8793fcff849bf962f1c0b5988dc37719e5f2e984
Olena Strypko <elenkastrypko@gmail.com>
2018-08-19 17:06:58 +0300

    Update task2.js

--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -14,7 +14,7 @@ if( SideALength<=0 || SideBLength<=0 || angle<=0 ){
     const SideCLength = Math.sqrt(mathOperation).toFixed(2);
     const square = Math.round(1/2*SideALength*SideBLength*Math.sin(angleConvert));
     const perimeter = +SideALength + +SideBLength + +SideCLength;
-    if(SideCLength == 0 || square == 0 || perimeter == 0){
+    if(SideCLength === 0 || square === 0 || perimeter === 0){
         result = `Invalid data`;
     }else{
         result = `c length: ${SideCLength}
